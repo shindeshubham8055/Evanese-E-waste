b94d37ed79bc5fcf9501339469f9f9a380fb6900
Your Name <you@example.com>
2024-11-24 10:59:57 +0530

    updated Response Entity

--- evanesce/src/main/java/com/evanesce/controller/UserController.java
+++ evanesce/src/main/java/com/evanesce/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.evanesce.controller;
 
-import java.util.List; 
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;  // Import ResponseEntity
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,67 +22,75 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping("/findbyphone")
-	public List<User> findByPhone(@RequestBody User user) {
-		System.out.println("\n@PostMapping(\"/findbyphone\")");
-		System.out.println("List<User> findByPhone(@RequestBody User user)");
-		return userService.findByPhone(user.getPhone());
-	}
-
 	// Login
 	@PostMapping("/login")
-	public List<User> getUserByEmailAndPassword(@RequestBody User user) {
+	public ResponseEntity<List<User>> getUserByEmailAndPassword(@RequestBody User user) {
 		System.out.println("\n@PostMapping(\"/login\")");
 		System.out.println("List<User> getUserByEmailAndPassword(@RequestBody User user)");
-		return userService.getUserByEmailAndPassword(user.getEmail(), user.getPassword());
+
+		List<User> users = userService.getUserByEmailAndPassword(user.getEmail(), user.getPassword());
+
+		if (users.isEmpty()) {
+			// If no users found, return a 404 NOT FOUND response
+			return ResponseEntity.notFound().build();
+		} else {
+			// If users found, return a 200 OK response with the list of users
+			return ResponseEntity.ok(users);
+		}
 	}
 
 	// Register
 	@PostMapping("/register")
-	public User insertUser(@RequestBody User user) {
+	public ResponseEntity<User> insertUser(@RequestBody User user) {
 		System.out.println("\n@PostMapping(\"/register\")");
 		System.out.println("public User insertUser(@RequestBody User user)");
-		return userService.insertUser(user);
+
+		User createdUser = userService.insertUser(user);
+
+		// If the user is successfully created, return a 201 CREATED response
+		return ResponseEntity.status(201).body(createdUser);
 	}
 
 	// Forget Password Mappings
-
 	@PostMapping("/findbyemail") // For Forgot Password 1
-	public List<User> findByEmail(@RequestBody User user) {
+	public ResponseEntity<List<User>> findByEmail(@RequestBody User user) {
 		System.out.println("\n@PostMapping(\"/findbyemail\")");
 		System.out.println("List<User> findByEmail(@RequestBody User user)");
-		return userService.findByEmail(user.getEmail());
+		List<User> users = userService.findByEmail(user.getEmail());
+		return ResponseEntity.ok(users);
 	}
 
 	@PostMapping("/forget") // For Forgot Password 2
-	public List<User> forgetPassword(@RequestBody User user) {
+	public ResponseEntity<List<User>> forgetPassword(@RequestBody User user) {
 		System.out.println("\n@PostMapping(\"/forget\")");
 		System.out.println("List<User> forgetPassword(@RequestBody User user)");
-		return userService.forgetPassword(user.getEmail(), user.getSecurityQues(), user.getSecurityAns());
+		List<User> users = userService.forgetPassword(user.getEmail(), user.getSecurityQues(), user.getSecurityAns());
+		return ResponseEntity.ok(users);
 	}
 
 	@PutMapping("/updatepassword") // For Forgot Password 3
-	public User updatePassword(@RequestBody User user) {
+	public ResponseEntity<User> updatePassword(@RequestBody User user) {
 		System.out.println("\n@PutMapping(\"/updatepassword\") ");
 		System.out.println("User updatePassword(@RequestBody User user)");
-		return userService.updatePassword(user);
+		User updatedUser = userService.updatePassword(user);
+		return ResponseEntity.ok(updatedUser);
 	}
 
-	//Admin Module - Delete User
+	// Admin Module - Delete User
 	@DeleteMapping("deleteuser/{uemail}")
-	public String deleteUser(@PathVariable String uemail) {
+	public ResponseEntity<String> deleteUser(@PathVariable String uemail) {
 		System.out.println("\n@DeleteMapping(\"deleteuser/{uemail}\")");
 		System.out.println("String deleteUser(@PathVariable String uemail)");
 		userService.deleteUser(uemail);
-		return "Deleted";
+		return ResponseEntity.ok("Deleted");
 	}
 
-	//Admin Module - Get All Users
+	// Admin Module - Get All Users
 	@GetMapping("/getallusers")
-	public List<User> getAllUsers(User user) {
+	public ResponseEntity<List<User>> getAllUsers() {
 		System.out.println("\n@GetMapping(\"/getallusers\")");
-		System.out.println("List<User> getAllUsers(User user) ");
-		return userService.getAllUsers();
+		System.out.println("List<User> getAllUsers() ");
+		List<User> users = userService.getAllUsers();
+		return ResponseEntity.ok(users);
 	}
-
 }
