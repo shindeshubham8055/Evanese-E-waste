2ca2514e83ea46ae813729a09ab832a4cc07f044
Your Name <you@example.com>
2024-11-25 12:06:59 +0530

    Standardize logging practices in RequestController

--- evanesce/src/main/java/com/evanesce/controller/RequestController.java
+++ evanesce/src/main/java/com/evanesce/controller/RequestController.java
@@ -1,6 +1,7 @@
 package com.evanesce.controller;
 
 import java.util.List;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,116 +19,89 @@ import com.evanesce.service.RequestService;
 @RestController
 public class RequestController {
 
+	private static final Logger logger = Logger.getLogger(RequestController.class.getName());
+
 	@Autowired
 	private RequestService requestService;
 
-	//User to Admins
 	@PostMapping("/requests")
 	public Request insertRequests(@RequestBody Request request) {
-		System.out.println("\n@PostMapping(\"/requests\")");
-		System.out.println("Request insertRequests(@RequestBody Request request)");
-		System.out.println(request.getQuantity());
+		logger.info("Handling POST request to insert a new request.");
+		logger.info("Quantity: " + request.getQuantity());
 		return requestService.insertRequests(request);
 	}
 
 	@GetMapping("/requests")
 	public List<Request> getAllRequests() {
-		System.out.println("\n@GetMapping(\"/requests\")");
-		System.out.println("List<Request> getAllRequests()");
+		logger.info("Handling GET request to retrieve all requests.");
 		return requestService.getAllRequests();
 	}
 
 	@GetMapping("/requests/{remail}")
 	public List<Request> getRequestsByEmail(@PathVariable String remail) {
-		System.out.println("\n@GetMapping(\"/requests/{remail}\")");
-		System.out.println("List<Request> getRequestsByEmail(@PathVariable String remail)");
+		logger.info("Handling GET request to retrieve requests by email: " + remail);
 		return requestService.getRequestsByEmail(remail);
 	}
 
 	@GetMapping("/getrequests/{rid}")
 	public List<Request> getRequestsById(@PathVariable int rid) {
-		System.out.println("\n@GetMapping(\"/getrequests/{rid}\")");
-		System.out.println("List<Request> getRequestsById(@PathVariable int rid)");
-
+		logger.info("Handling GET request to retrieve request by ID: " + rid);
 		return requestService.getRequestById(rid);
 	}
 
 	@PostMapping("/pendingrequests")
 	public List<Request> getPendingRequests(@RequestBody Request request) {
-		System.out.println("\n@PostMapping(\"/pendingrequests\")");
-		System.out.println("List<Request> getPendingRequests(@RequestBody Request request)");
-		System.out.println("{\r\n"
-				+ "    \"email\":\"user@gmail.com\",\r\n"
-				+ "    \"boolean\":\"true\"\r\n"
-				+ "}");
+		logger.info("Handling POST request to retrieve pending requests for email: " + request.getEmail());
 		return requestService.pendingRequests(request.getEmail(), false);
 	}
 
 	@GetMapping("/viewallpendingrequests")
 	public List<Request> getPendingRequests(boolean status) {
-		System.out.println("\n@GetMapping(\"/viewallpendingrequests\")");
-		System.out.println("List<Request> getPendingRequests(boolean status)");
+		logger.info("Handling GET request to view all pending requests.");
 		return requestService.viewAllPendingRequests(false);
 	}
 
 	@GetMapping("/viewcollections")
 	public List<Request> viewAllDonations(boolean status) {
-		System.out.println("\n@GetMapping(\"/viewcollections\")");
-		System.out.println("List<Request> viewAllDonations(boolean status)");
+		logger.info("Handling GET request to view all donations.");
 		return requestService.viewAllDonations(status);
 	}
 
 	@PostMapping("/viewdonations")
 	public List<Request> viewDonations(@RequestBody Request request) {
-		System.out.println("\n@PostMapping(\"/viewdonations\")");
-		System.out.println(" List<Request> viewDonations(@RequestBody Request request)");
-		System.out.println("By Default True => Send EmailID");
+		logger.info("Handling POST request to view donations for email: " + request.getEmail());
 		return requestService.viewDonations(request.getEmail(), true);
 	}
 
-	//Agent Module request Card
 	@PutMapping("/requests/{id}")
 	public Request updateRequests(@PathVariable int id, @RequestBody Request request) {
-		
-		System.out.println("\n@PutMapping(\"/requests/{id}\")");
-		System.out.println("Request updateRequests(@PathVariable int id, @RequestBody Request request)");
-		System.out.println("IN UPDATE PAGE");
-		System.out.print("In update Request : Agent id " + id + " Request " + request);
+		logger.info("Handling PUT request to update request with ID: " + id);
 		return requestService.updateRequests(id, request);
 	}
 
 	@PutMapping("/pendingrequestbyUser")
 	public Request updateRequestsByDonor(@RequestBody Request request) {
-		System.out.println("\n@PutMapping(\"/pendingrequestbyUser\")");
-		System.out.println("Request updateRequestsByDonor(@RequestBody Request request)");
+		logger.info("Handling PUT request to update pending request by donor.");
 		return requestService.updatePendingRequestByUser(request);
 	}
 
 	@PutMapping("/requestspay/{reqid}")
 	public Request updateRequests(@PathVariable String reqid, @RequestBody Request request) {
-		
-		System.out.println("\n@PutMapping(\"/requestspay/{reqid}\")");
-		System.out.println("Request updateRequests(@PathVariable String reqid, @RequestBody Request request)");
-		System.out.print("In update Request : id" + reqid + " Request " + request);
+		logger.info("Handling PUT request to update payment for request ID: " + reqid);
 		return requestService.updatePayment(request, Integer.parseInt(reqid));
 	}
 
 	@DeleteMapping("/requests/{rid}")
 	public String deleteRequest(@PathVariable int rid) {
-		System.out.println("\n@DeleteMapping(\"/requests/{rid}\")");
-		System.out.println("String deleteRequest(@PathVariable int rid)");
+		logger.info("Handling DELETE request to remove request with ID: " + rid);
 		requestService.deleteRequest(rid);
 		return "Deleted";
 	}
 
 	@GetMapping("/imageuploadstatus/{imgreqid}")
 	public boolean imageuploadstatus(@PathVariable String imgreqid) {
-		System.out.println("\n@GetMapping(\"/imageuploadstatus/{imgreqid}\")");
-		System.out.println("boolean imageuploadstatus(@PathVariable String imgreqid)");
-		System.out.println("http://localhost:8080/imageuploadstatus/52");
-		int img_req_id = Integer.parseInt(imgreqid);
-		return requestService.checkImageUploadStatus(img_req_id);
-
+		logger.info("Handling GET request to check image upload status for request ID: " + imgreqid);
+		int imgReqId = Integer.parseInt(imgreqid);
+		return requestService.checkImageUploadStatus(imgReqId);
 	}
-
 }
